e4d07c8cb75174939198b4e0a01cdb1d710df752
Quan <n.dinh@tdsoftware.de>
2019-05-16 17:39:49 +0200

    update 4.1

--- src/infovis/diagram/layout/Fisheye.java
+++ src/infovis/diagram/layout/Fisheye.java
@@ -8,37 +8,32 @@ import infovis.diagram.elements.Vertex;
 public class Fisheye implements Layout{
 	protected final int magnificationFactor = 4;
 
-	public void setMouseCoords(int x, int y, View view) {
-		// TODO Auto-generated method stub
-	}
+	public void setMouseCoords(int x, int y, View view) { }
 
 	public Model transform(Model model, View view) {
 		Model fishEyeModel = new Model();
 		for (Element element : model.getElements()) {
 			Vertex current = (Vertex) element;
 			double ratio = current.getWidth() / current.getHeight();
-			double boundaryX = current.getX() + current.getWidth();
+			double boundsX = current.getX() + current.getWidth();
 			double topLeftX = getFisheyeCoordinate(current.getX(), view.getWidth(), view.getFocusX());
-			double rightX = getFisheyeCoordinate(boundaryX, view.getWidth(), view.getFocusX());
-			double boundaryY = current.getY() + current.getHeight();
+			double rightX = getFisheyeCoordinate(boundsX, view.getWidth(), view.getFocusX());
+			double boundsY = current.getY() + current.getHeight();
 			double topLeftY = getFisheyeCoordinate(current.getY(), view.getHeight(), view.getFocusY());
-			double bottomY = getFisheyeCoordinate(boundaryY, view.getHeight(), view.getFocusY());
+			double bottomY = getFisheyeCoordinate(boundsY, view.getHeight(), view.getFocusY());
 			double width = rightX - topLeftX;
 			double height = bottomY - topLeftY;
 			double heightByWidth = width / ratio;
 			double widthByHeight = height * ratio;
-
 			// Select minimum size and preserve ratio
-			if (width < widthByHeight) {
+			if (width < widthByHeight)
 				height = heightByWidth;
-			} else {
+			else
 				width = widthByHeight;
-			}
-
 			Vertex newVertex = new Vertex(topLeftX, topLeftY, width, height);
 			fishEyeModel.addVertex(newVertex);
 		}
-		return null;
+		return fishEyeModel;
 	}
 
 	private double getFisheyeCoordinate(double original, double boundary, double focus) {
